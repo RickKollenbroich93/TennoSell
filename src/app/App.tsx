488650a273426acc9413c6e875f88bf23bffd7db
Rick <rick.kollenbroich93@googlemail.com>
2022-01-11 16:42:36 +0100

    Fix styling issue

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,30 @@ import { useState } from 'react';
 import HomeIcon from './components/assets/HomeIcon';
 import Button from './components/Button/Button';
 import Searchbar from './components/Searchbar/Searchbar';
+import Card from './components/Card/Card';
 
 function App(): JSX.Element {
   const [searchValue, setSearchValue] = useState<string>('');
+  const mockCard = [
+    {
+      item: 'Mirage',
+      itemImg: 'URL',
+      itemSell: 2,
+      itemBuy: 12,
+    },
+    {
+      item: 'Mirage',
+      itemImg: 'URL',
+      itemSell: 2,
+      itemBuy: 12,
+    },
+    {
+      item: 'Mirage',
+      itemImg: 'URL',
+      itemSell: 2,
+      itemBuy: 12,
+    },
+  ];
   return (
     <div className={styles.appcontainer}>
       <h1>Tenno Sell</h1>
@@ -28,6 +49,16 @@ function App(): JSX.Element {
         </div>
       </nav>
       <h2>Top Items</h2>
+      <div className={styles.cardcontainer}>
+        {mockCard.map((card) => (
+          <Card
+            item={card.item}
+            itemImg={card.itemImg}
+            itemBuy={card.itemBuy}
+            itemSell={card.itemSell}
+          />
+        ))}
+      </div>
     </div>
   );
 }
