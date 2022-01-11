eb28e225d24e49c3a5af1f2dd87555351b391ab8
Rick <rick.kollenbroich93@googlemail.com>
2022-01-11 15:51:31 +0100

    Create Nav

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,24 @@
 import styles from './App.module.css';
+import HomeIcon from './components/assets/HomeIcon';
+import Button from './components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <div className={styles.appcontainer}>
-      <div className={styles.img}>
-        {/* <img src="src/app/components/assets/Background.svg" /> */}
-      </div>
       <h1>Tenno Sell</h1>
+      <nav className={styles.navcontainer}>
+        <div className={styles.buttoncontainer}>
+          <Button>Prime Parts</Button>
+        </div>
+        <div className={styles.buttoncontainer}>
+          <Button>
+            <HomeIcon />
+          </Button>
+        </div>
+        <div className={styles.buttoncontainer}>
+          <Button>Mods</Button>
+        </div>
+      </nav>
     </div>
   );
 }
