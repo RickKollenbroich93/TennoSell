1b6e4601735b57117f8dbe1663eb7ea5ba6ee147
Rick <rick.kollenbroich93@googlemail.com>
2022-01-11 16:07:53 +0100

    add searchbar and change color for it

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
 import styles from './App.module.css';
+import { useState } from 'react';
 import HomeIcon from './components/assets/HomeIcon';
 import Button from './components/Button/Button';
+import Searchbar from './components/Searchbar/Searchbar';
 
 function App(): JSX.Element {
+  const [searchValue, setSearchValue] = useState<string>('');
   return (
     <div className={styles.appcontainer}>
       <h1>Tenno Sell</h1>
+      <Searchbar
+        type="search"
+        searchValue={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
       <nav className={styles.navcontainer}>
         <div className={styles.buttoncontainer}>
           <Button>Prime Parts</Button>
@@ -19,6 +27,7 @@ function App(): JSX.Element {
           <Button>Mods</Button>
         </div>
       </nav>
+      <h2>Top Items</h2>
     </div>
   );
 }
